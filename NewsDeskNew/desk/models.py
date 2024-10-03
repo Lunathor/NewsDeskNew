34b9af30be414528a27fbeec7b477a717c3bbf16
Lunathor <mrcoopgmz@gmail.com>
2024-10-03 14:22:11 +0300

    Models in final version

--- NewsDeskNew/desk/models.py
+++ NewsDeskNew/desk/models.py
@@ -31,4 +31,45 @@ class User(AbstractUser):
     
     def __str__(self):
         return f'{self.username}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок поста')
+    text = models.TextField(verbose_name='Содержание поста')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор поста')
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICE, verbose_name='Категория поста')
+    datecreation = models.DateTimeField(auto_now_add=True)
+    
+    def preview(self):
+        return self.text[0:123] + '...'
+    
+    def category_name(self):
+        for category in CATEGORY_CHOICE:
+            if self.category == category[0]:
+                return category[1]
+            
+    def __str__(self):
+        return f'{self.title.title()}'
+    
+    
+class Image(models.Model):
+    file = models.ImageField(upload_to='desk/gallery/images', verbose_name='Картинка')
+    post = ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    
+    
+class Video(models.Model):
+    url = EmbedVideoField(verbose_name="Ссылка на Видео")
+    post = ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор поста')
+    text = models.TextField(verbose_name='Содержание комментария')
+    datecreation = models.DateTimeField(auto_now_add=True)
+    is_confirmed = models.BooleanField(default=False, verbose_name='Подтвержденный комментарий')
+    
+    def __str__(self):
+        return f'{self.text}'
+    
     
