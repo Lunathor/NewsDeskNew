48548b77dcee39b4a2f41479b675dd0c8b7db8e0
Lunathor <mrcoopgmz@gmail.com>
2024-10-04 23:15:27 +0300

    Сделал ссылки нужные в шаблонах дальше буду допиливать шаблоны и создам приложение для регистрации accounts

--- NewsDeskNew/desk/urls.py
+++ NewsDeskNew/desk/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', PostListView.as_view(), name='MainPage'),
     path('post/<int:pk>/', post_review, name='PostReview'),
+    path('post/create/', PostCreateView.as_view(), name='Create'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='Update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='Delete')
 ]
