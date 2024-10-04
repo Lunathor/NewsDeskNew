8dd48044e6c3ba349da6e9b714ca76736807045a
Lunathor <mrcoopgmz@gmail.com>
2024-10-04 23:44:57 +0300

    Ссылки для шаблонов Delete.html и Create.html

--- NewsDeskNew/desk/urls.py
+++ NewsDeskNew/desk/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', post_review, name='PostReview'),
     path('post/create/', PostCreateView.as_view(), name='Create'),
     path('post/update/<int:pk>', PostUpdateView.as_view(), name='Update'),
-    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='Delete')
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='Delete'),
+    path('post/delete_image/<int:pk>', delete_image, name='delete_image'),
+    path('post/delete_video/<int:pk>', delete_video(), name='delete_video'),
 ]
