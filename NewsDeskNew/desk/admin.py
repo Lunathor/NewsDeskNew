d69b368eef29c034d5277daf97cc3b38e03ecec5
Lunathor <mrcoopgmz@gmail.com>
2024-10-04 22:46:31 +0300

    PostReview template some filters for it and models added to admin.py

--- NewsDeskNew/desk/admin.py
+++ NewsDeskNew/desk/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from multiprocessing.reduction import register
+from .models import *
+
 
 # Register your models here.
+
+
+class ImageLine(admin.TabularInline):
+    model = Image
+    extra = 1
+    max_num = 5
+
+
+class VideoLine(admin.TabularInline):
+    model = Video
+    extra = 1
+    max_num = 2
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('pk', 'title', 'text', 'author', 'category')
+    inlines = [ImageLine, VideoLine]
+
+
+class ImageAdmin(admin.ModelAdmin):
+    model = Image
+    list_display = ('pk', 'file')
+    
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Video)
+admin.site.register(Comment)
+admin.site.register(User)
--- NewsDeskNew/desk/templatetags/custom_filters.py
+++ NewsDeskNew/desk/templatetags/custom_filters.py
@@ -11,7 +11,26 @@ def is_image_exist(value: Post) -> bool:
     else:
         return False
     
-    
+
+@register.filter
 def preview(value: Post) -> Image:
     image = Image.objects.filter(post=value).first()
     return image.file.url
+
+
+@register.filter
+def get_image(value: Post) -> list[Image]:
+    return Image.objects.filter(post=value.pk).all()
+
+
+@register.filter
+def is_video_exist(value: Post) -> bool:
+    if Video.objects.filter(post=value.pk).first():
+        return True
+    else:
+        return False
+
+
+@register.filter
+def get_video(value: Post) -> list[Video]:
+    return Video.objects.filter(post=value.pk).all()
