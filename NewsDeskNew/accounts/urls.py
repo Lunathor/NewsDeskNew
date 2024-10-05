48271fade66038a1b75ab30b9d8e6093ae3227b0
Lunathor <mrcoopgmz@gmail.com>
2024-10-05 23:39:31 +0300

    Проверка личного кабинета, 2 версии custom_filters.py конфликтовали поэтому пришлось объединить. Внутри представления почему то не отклоняются комментарии

--- NewsDeskNew/accounts/urls.py
+++ NewsDeskNew/accounts/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('signup/', SignUp, name='SignUp'),
     path('verification/<int:user_id>', Verification, name='Verification'),
-    path('personalcabinet/', PersonalCabinetView, name='PersonalCabinet'),
+    path('personalcabinet/', PersonalCabinetView.as_view(), name='PersonalCabinet'),
     path('user/', include('django.contrib.auth.urls')),
     path('user/logout/', Logout, name='Logout'),
 ]
